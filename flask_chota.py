4547f802d43c47fb0915e158eb4510a3f3097395
adiboy6 <aditya.reddy.97@gmail.com>
2019-04-30 14:45:28 +0530

    added for multiple boards

--- flask_chota.py
+++ flask_chota.py
@@ -6,6 +6,10 @@ import urllib
 
 app = Flask(__name__)
 
+hire=[{'onboarding','auth-server'},'5cc7d40ccb79a267e1e5644f']
+engage=[{'data-processor','ats'},'5cc7d42140535067a619d36b']
+nurture=[{'starfox','survey'},'5cc7d417e375a43d1a8bed84']
+
 @app.route("/trello", methods=["POST"])
 def sendtoTrello():
 	data1=request.json
@@ -19,7 +23,16 @@ def sendtoTrello():
 				card_name=(i['fields']['app_name']+"-"+i['fields']['caller_file_name'])
 		else:
 			card_name=(i['fields']['app_name']+"-"+i['fields']['logger_name'][i['fields']['logger_name'].rfind(".")+1:])
-		os.system("sh curl_to_trello.sh "+card_name+" "+message)
+		
+		idList=''
+		if i['fields']['app_name'] in hire[0]:
+			idList=hire[1]
+		elif i['fields']['app_name'] in engage[0]:
+			idList=engage[1]
+		else:
+			idList=nurture[1]
+
+		os.system("sh curl_to_trello.sh "+card_name+" "+message+" "+idList)
 		print("\n\n")
 	return "Request Processed.\n"
 
