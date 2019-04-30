9e15de66199f80303d7fc17b6b45f8793b9ad588
adiboy6 <aditya.reddy.97@gmail.com>
2019-04-30 16:09:26 +0530

    Update curl_to_trello.sh

--- curl_to_trello.sh
+++ curl_to_trello.sh
@@ -1,9 +1,11 @@
 card_name=$1
 card_desc=$2
 list_id=$3
-count=`curl --request GET   --url 'https://api.trello.com/1/lists/'$list_id'/cards?key=f6dff15a20b77f79814e8331f507adc5&token=b7659470f6f04e8d31d8b3cb3807994d30aed92ed64696fd99bfbf8a4ded4a8a' | grep -c "$card_name"`
+api_key=''
+api_token=''
+count=`curl --request GET   --url 'https://api.trello.com/1/lists/'$list_id'/cards?key='$api_key'&token='$api_token'' | grep -c "$card_name"`
 if [ $count -eq 0 ]
 then
 curl --request POST \
-  --url 'https://api.trello.com/1/cards?name='$card_name'&desc='$card_desc'&pos=top&idList='$list_id'&keepFromSource=all&key=f6dff15a20b77f79814e8331f507adc5&token=b7659470f6f04e8d31d8b3cb3807994d30aed92ed64696fd99bfbf8a4ded4a8a'
+  --url 'https://api.trello.com/1/cards?name='$card_name'&desc='$card_desc'&pos=top&idList='$list_id'&keepFromSource=all&key='$api_key'&token='$api_token''
   fi
